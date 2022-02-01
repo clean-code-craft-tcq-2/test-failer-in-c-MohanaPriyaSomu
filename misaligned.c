168d10e2cdc9491367fe76398b035b54481dd9e2
MohanaPriyaSomu <91050383+MohanaPriyaSomu@users.noreply.github.com>
2022-02-01 17:53:40 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -8,7 +8,7 @@ const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 
 struct colorPair
 {
-	int PairNo;
+	int pairNo;
 	char MajorColor[8];
 	char MinorColor[8];
 };
@@ -23,11 +23,11 @@ int printColorMap() {
         for(j = 0; j < 5; j++)
 		{
 			
-			Pair[ArrayIndex].Number = i * 5 + j;
+			Pair[ArrayIndex].pairNo = i * 5 + j;
 			strcpy(Pair[ArrayIndex].MajorColor , majorColor[i]);
 			strcpy(Pair[ArrayIndex].MinorColor , minorColor[i]);
 			printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);	
-		    ArrayIndex++;				          
+		        ArrayIndex++;				          
         }
     }
     return i * j;
@@ -36,7 +36,7 @@ int printColorMap() {
 int main() {
     int result = printColorMap();
     assert(result == 25);
-	assert(Pair[10].Number == 10);	
+	assert(Pair[10].pairNo == 10);	
 	assert(strcmp(Pair[10].MajorColor, "Red") == 0);
 	assert(strcmp(Pair[10].MinorColor, "Slate") == 0);
     printf("All is well (maybe!)\n");
